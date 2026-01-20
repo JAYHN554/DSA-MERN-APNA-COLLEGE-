435ba9e264844abd83b70fd77aaa2c1fdd9b6c33
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-20 16:52:16 +0530

    max

---
+++ DSA/DAY 1/max.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class max {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of a and b and c");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        if((a>b) && (a>c)){
+            System.out.println("Max number is "+a);
+        }
+        else if((b>c) && (b>a)){
+            System.out.println("Max number is "+b);   
+        }
+        else
+        System.out.println("Max number is "+c);
+
+    }
+}
