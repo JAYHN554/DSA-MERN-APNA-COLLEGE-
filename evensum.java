c30187618868ae3af5f4e277b955e7cf410e5e54
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-21 13:37:50 +0530

    even

---
+++ evensum.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class evensum {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        n=in.nextInt();
+        n=n*(n+1);
+        System.out.println("Sum of even natural number is "+n);
+    }
+}
