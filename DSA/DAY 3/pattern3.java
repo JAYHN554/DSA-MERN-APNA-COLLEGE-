3fafb28cec5beb2fcbea198f0f6bdcc4048fe2db
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-21 20:30:10 +0530

    pattern

---
+++ DSA/DAY 3/pattern3.java
@@ -0,0 +1,19 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+// The pattern
+// 54321
+// 5432
+// 543
+// 54
+// 5
