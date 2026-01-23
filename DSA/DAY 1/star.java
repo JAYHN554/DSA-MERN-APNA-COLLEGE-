8c445719934aa543c2cbcaa523b664480e00892e
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-23 16:45:09 +0530

    sa

---
+++ DSA/DAY 1/star.java
@@ -0,0 +1,11 @@
+public class star {
+    public static void main(String args[])
+    {   
+        System.out.println("The pattern is:");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
+//  My first code in java
