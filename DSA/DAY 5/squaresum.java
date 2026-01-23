ec1509b5424767a3f02d45b96d5101f0ab5ac001
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-23 15:25:36 +0530

    sum

---
+++ DSA/DAY 5/squaresum.java
@@ -0,0 +1,10 @@
+public class squaresum {
+    public static void main(String[] args) {
+        int n=8,sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum=sum+(i*i);
+        }
+        System.out.println("Sum= "+sum);
+    }
+}
