ed339656e1d1d22436599147947527ec7b19e530
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-29 20:20:44 +0530

    casting

---
+++ DSA/DAY 7/casting.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class casting {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        float a=10.2225f;        
+        int b=(int) a;
+        System.out.println(b);
+    }
+}
