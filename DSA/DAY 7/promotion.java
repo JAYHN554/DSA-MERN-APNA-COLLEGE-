12a52438391a1b93a059477297853ea774a2a42d
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-29 21:04:23 +0530

    add

--- DSA/DAY 7/promotion.java
+++ DSA/DAY 7/promotion.java
@@ -1,9 +1,14 @@
 public class promotion {
     public static void main(String[] args) {
-        int ch='a';
-        short a=50;
-        int sum=ch+a;
-        System.out.println((int)(ch));
+        // int ch='a';
+        // short a=50;
+        // int sum=ch+a;
+        // System.out.println((int)(ch));
+        // System.out.println(sum);
+        int a=10;
+        float f=10.56;
+        long c=25;
+        double ans=a+b+c;
         System.out.println(sum);
     }
 }
