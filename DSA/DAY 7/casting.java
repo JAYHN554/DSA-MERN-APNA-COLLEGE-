bbcef31eb7da7d50501845db4e1b7afab5d4aa91
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-29 20:21:54 +0530

    update

--- DSA/DAY 7/casting.java
+++ DSA/DAY 7/casting.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 public class casting {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        float a=10.2225f;        
-        int b=(int) a;
-        System.out.println(b);
+        // float a=10.2225f;        
+        // int b=(int) a;
+        // System.out.println(b);
+
+        float marks = 99.99f;
+        int marks2= (int)marks;
+        System.out.println(marks2);
     }
 }
