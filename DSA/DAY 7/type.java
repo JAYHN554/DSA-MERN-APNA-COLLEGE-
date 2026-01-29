4800cf5808bcefeebdb50094fe8cd8cda968a7de
JAY NAKASHE <140512348+JAYHN554@users.noreply.github.com>
2026-01-29 20:10:01 +0530

    type conversion

---
+++ DSA/DAY 7/type.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class type {
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner (System.in);
+        // int a=25;
+        // long b=a;
+        // long a =5;
+        // // int b=a;
+        // System.err.println(b);
+    }
+
+}
+// incompatible types: possible lossy conversion from long to int
